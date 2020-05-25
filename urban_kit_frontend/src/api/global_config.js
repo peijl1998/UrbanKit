b0cefbf4314d8346169c00b514cda7e611e24809
HelloSilicat <453715647pjl@gmail.com>
2020-05-26 03:05:48 +0800

    mask README

--- urban_kit_frontend/src/api/global_config.js
+++ urban_kit_frontend/src/api/global_config.js
@@ -1,8 +1,8 @@
 // Map Setting
 var dot_shape="circle";
 var dot_size=10;
-//var backend="39.97.247.132:8000";
-var backend="localhost:8000";
+var backend="39.97.247.132:8000";
+//var backend="localhost:8000";
 
 
 
